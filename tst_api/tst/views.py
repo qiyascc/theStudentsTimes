cde1f0a33fdfe37dca245658d06ed30d5f23d8e0
Qiyas <107604130+qiyascc@users.noreply.github.com>
2024-04-22 17:37:20 +0400

    For exceptions

--- tst_api/tst/views.py
+++ tst_api/tst/views.py
@@ -6,6 +6,7 @@ from .models import Tst
 from .serializers import TstSerializer
 from django.db.models import F
 import calendar
+from rest_framework.exceptions import APIException
 
 class TstViewSet(viewsets.ModelViewSet):
     queryset = Tst.objects.all()
@@ -13,21 +14,30 @@ class TstViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, url_path='year/(?P<year>[0-9]{4})')
     def year(self, request, year=None):
-        gazette = self.queryset.filter(month__year=year)
-        serializer = self.get_serializer(gazette, many=True)
-        return Response(serializer.data)
-    
+        try:
+            gazette = self.queryset.filter(month__year=year)
+            serializer = self.get_serializer(gazette, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            raise APIException(str(e))
+
     @action(detail=False, url_path='year')
     def list_years(self, request):
-        years = Tst.objects.annotate(year=F('month__year')).values('year').distinct()
-        year_list = [{'year': y['year'], 'url': request.build_absolute_uri(str(y['year']) + '/')} for y in years]
-        return Response(year_list)
+        try:
+            years = Tst.objects.annotate(year=F('month__year')).values('year').distinct()
+            year_list = [{'year': y['year'], 'url': request.build_absolute_uri(str(y['year']) + '/')} for y in years]
+            return Response(year_list)
+        except Exception as e:
+            raise APIException(str(e))
 
     @action(detail=False, url_path='year/(?P<year>[0-9]{4})/month')
     def list_months(self, request, year=None):
-        months = Tst.objects.filter(month__year=year).annotate(month_number=F('month__month')).values('month_number').distinct()
-        month_list = [{'month': calendar.month_name[m['month_number']], 'url': request.build_absolute_uri(calendar.month_name[m['month_number']] + '/')} for m in months]
-        return Response(month_list)
+        try:
+            months = Tst.objects.filter(month__year=year).annotate(month_number=F('month__month')).values('month_number').distinct()
+            month_list = [{'month': calendar.month_name[m['month_number']], 'url': request.build_absolute_uri(calendar.month_name[m['month_number']] + '/')} for m in months]
+            return Response(month_list)
+        except Exception as e:
+            raise APIException(str(e))
     
     @action(detail=False, url_path='year/(?P<year>[0-9]{4})/month/(?P<month>[A-Za-z]+|[0-9]{1,2})')
     def month(self, request, year=None, month=None):
@@ -39,14 +49,16 @@ class TstViewSet(viewsets.ModelViewSet):
             gazette = self.queryset.filter(month__year=year, month__month=month_number)
             serializer = self.get_serializer(gazette, many=True)
             return Response(serializer.data)
-        except (ValueError, IndexError):
-            return Response({"error": "Invalid month name or number"}, status=400)
+        except Exception as e:
+            raise APIException(str(e))
         
     @action(detail=False, url_path='editor')
     def editor(self, request):
-        editors = self.queryset.values('editor', 'month')
-        return Response(editors)
-
+        try:
+            editors = self.queryset.values('editor', 'month')
+            return Response(editors)
+        except Exception as e:
+            raise APIException(str(e))
 
 def api_home(request):
     return render(request, 'api.html')
